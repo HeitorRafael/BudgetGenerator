36a5490d15a0597c14f6a09a806121d2ed0f5e4e
HeitorRafael <hitoor.d@gmail.com>
2025-05-27 14:06:39 -0300

    a

---
+++ projeto-poo/src/java/api/LogoutServlet.java
@@ -0,0 +1,34 @@
+package api;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author raffinoh
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Invalida a sessão
+        HttpSession session = request.getSession(false); // evita criar nova se não existir
+        if (session != null) {
+            session.invalidate();
+        }
+
+        // Redireciona para a tela de login
+        response.sendRedirect("index.html");
+    }
+}
