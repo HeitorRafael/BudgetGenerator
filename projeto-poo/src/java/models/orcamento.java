b7b897a87ef393611fd7fef6818bcb455cde6080
HeitorRafael <hitoor.d@gmail.com>
2025-06-03 16:22:21 -0300

    orcamento . java atualizado

--- projeto-poo/src/java/models/orcamento.java
+++ projeto-poo/src/java/models/orcamento.java
@@ -3,42 +3,80 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package models;
-import java.util.UUID;
-/**
- *
- * @author raffinoh
- */
 
+import java.util.UUID;
 
 public class orcamento {
     private String id;
-    private String descricao;
-    public double valor;
+    private String tipo; // "produto" ou "servico"
+    // Campos para produto
+    private String nomeProduto;
+    private String materiais;
+    private double custo;
+    private double lucro; // em porcentagem
+
+    // Campos para serviço
+    private String descricaoServico;
+    private double horas;
+    private double valorHora;
+    private double custosExtras;
+
+    // Comum
+    private double valorFinal; // valor calculado
+    private String respostaIA; // texto gerado pela IA
     private boolean comMarcaDagua;
 
-    public orcamento(String descricao, double valor) {
+    public orcamento(String tipo) {
         this.id = UUID.randomUUID().toString();
-        this.descricao = descricao;
-        this.valor = valor;
-        this.comMarcaDagua = true; // padrão vem com marca d'água
+        this.tipo = tipo;
+        this.comMarcaDagua = true;
     }
 
-    public void removerMarcaDagua() {
-        this.comMarcaDagua = false;
-    }
+    // Getters e setters
 
-    public String getId() {
-        return id;
-    }
+    public String getId() { return id; }
+    public String getTipo() { return tipo; }
+    public boolean isComMarcaDagua() { return comMarcaDagua; }
+    public void removerMarcaDagua() { this.comMarcaDagua = false; }
 
-    public boolean isComMarcaDagua() {
-        return comMarcaDagua;
-    }
+    // Produto
+    public String getNomeProduto() { return nomeProduto; }
+    public void setNomeProduto(String nomeProduto) { this.nomeProduto = nomeProduto; }
+    public String getMateriais() { return materiais; }
+    public void setMateriais(String materiais) { this.materiais = materiais; }
+    public double getCusto() { return custo; }
+    public void setCusto(double custo) { this.custo = custo; }
+    public double getLucro() { return lucro; }
+    public void setLucro(double lucro) { this.lucro = lucro; }
+
+    // Serviço
+    public String getDescricaoServico() { return descricaoServico; }
+    public void setDescricaoServico(String descricaoServico) { this.descricaoServico = descricaoServico; }
+    public double getHoras() { return horas; }
+    public void setHoras(double horas) { this.horas = horas; }
+    public double getValorHora() { return valorHora; }
+    public void setValorHora(double valorHora) { this.valorHora = valorHora; }
+    public double getCustosExtras() { return custosExtras; }
+    public void setCustosExtras(double custosExtras) { this.custosExtras = custosExtras; }
+
+    // Comum
+    public double getValorFinal() { return valorFinal; }
+    public void setValorFinal(double valorFinal) { this.valorFinal = valorFinal; }
+    public String getRespostaIA() { return respostaIA; }
+    public void setRespostaIA(String respostaIA) { this.respostaIA = respostaIA; }
 
     @Override
     public String toString() {
-        return "Descrição: " + descricao + "\n" +
-               "Valor: R$ " + valor + "\n" +
-               (comMarcaDagua ? "[ORÇAMENTO COM MARCA D'ÁGUA]" : "[ORÇAMENTO LIVRE]");
+        if ("produto".equals(tipo)) {
+            return "Produto: " + nomeProduto + "\nMateriais: " + materiais + "\nCusto: R$" + custo +
+                   "\nLucro: " + lucro + "%\nValor Final: R$" + valorFinal +
+                   "\n" + (comMarcaDagua ? "[COM MARCA D'ÁGUA]" : "[LIVRE]") +
+                   "\nOrçamento IA:\n" + respostaIA;
+        } else {
+            return "Serviço: " + descricaoServico + "\nHoras: " + horas + "\nValor/Hora: R$" + valorHora +
+                   "\nCustos Extras: R$" + custosExtras + "\nValor Final: R$" + valorFinal +
+                   "\n" + (comMarcaDagua ? "[COM MARCA D'ÁGUA]" : "[LIVRE]") +
+                   "\nOrçamento IA:\n" + respostaIA;
+        }
     }
 }
