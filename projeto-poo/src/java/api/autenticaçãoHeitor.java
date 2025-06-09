80b12c063040ebd3c80e1c86cb5470e961969e8e
KauanHCS <Kauanhenrike78@gmail.com>
2025-06-09 18:22:29 -0300

    Exclusão de arquivos inuteis

--- projeto-poo/src/java/api/autenticaçãoHeitor.java
+++
@@ -1,5 +0,0 @@
-//HttpSession session = request.getSession(false);
-//if (session == null || session.getAttribute("usuario") == null) {
-//    response.sendRedirect("index.html?erro=autenticacao");
-//    return;
-//}
--- projeto-poo/src/java/utilities/DBConfig.java
+++
@@ -1,5 +0,0 @@
-package utilities;
-
-public class DBConfig {
-    public static final String URL = "jdbc:sqlite:/caminho/para/budgetdb.sqlite"; // Altere o caminho conforme seu ambiente
-}
